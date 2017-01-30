af2831bdb2fb693ad6f8d121fa49059a27788179
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-01-30 11:39:19 +0100

    add migrations

--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -13,6 +13,7 @@ namespace CodeFirstNewDatabaseSample
         {
             public int BlogId { get; set; }
             public String Name { get; set; }
+            public string Url { get; set; }
 
             public virtual List<Post> Posts { get; set; }
         }
